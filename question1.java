4f026c744374217e137b85ac4905fdebbfdd5d74
Kushagradixit03 <kushagradixit0312@gmail.com>
2025-01-18 15:42:48 +0530

    question1 is added

---
+++ question1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class question1{ public static void main(String Args[]){
+Scanner sc= new Scanner (System.in);
+int a=sc.nextInt();
+switch(a){
+case 1:
+System.out.println("one");
+break;
+case 2:
+System.out.println("two");
+break;
+case 3:
+System.out.println("three");
+break;
+case 4:
+System.out.println("four");
+break;
+
+case 5:
+System.out.println("five");
+break;
+case 6:
+System.out.println("six");
+break;
+case 7:
+System.out.println("seven");
+break;
+case 8:
+System.out.println("eight");
+break;
+case 9:
+System.out.println("nine");
+break;
+case 10:
+System.out.println("ten");
+break;
+default:
+System.out.println("enter number between 1-10");}
+}
+}
